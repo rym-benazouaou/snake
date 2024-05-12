a0f3365259f3c378c89822bc8f7baad94915948e
Rym BENAZOUAOU <12309531@f203-16.ig-edu.univ-paris13.fr>
2024-05-12 18:14:00 +0200

    tirer fruit s'il est mange

--- main.c
+++ main.c
@@ -19,36 +19,43 @@ int m = atoi(argv[2]);
 	struct grille * jeu_grille = allouer_grille( n , m);
 	struct tete_serpent *serpent = cree_serpent(n,m);
 	int nouvelle_direction;
+	int tirage_necessaire=0;
 	
-	
+	grille_tirage_fruit (jeu_grille);
 	while(1){
-		/*system("clear");*/
 		printf("\33[2J");
 		grille_vider (jeu_grille);
-		grille_tirage_fruit (jeu_grille);
 		grille_remplir(jeu_grille,serpent);
 		grille_redessiner(jeu_grille);
-		ajouter_element_serpent(serpent);
-		srand(time(NULL));
-		nouvelle_direction = get_direction() ;
-		/*ignorer les demi tour */
-		switch(nouvelle_direction){
-			case 0:
-				if(serpent -> direction ==3) nouvelle_direction =3;
-				break;
-			case 1:
-				if(serpent -> direction ==2) nouvelle_direction =2;
-				break;
-			case 2:
-				if(serpent -> direction ==1) nouvelle_direction =1;
-				break;
-			case 3:
-				if(serpent -> direction ==0) nouvelle_direction =0;
-				break;
-			default : 
-				nouvelle_direction= serpent ->direction;
+		if((serpent ->position_x == jeu_grille -> fruitX) && (serpent ->position_y == jeu_grille -> fruitY)){
+			ajouter_element_serpent(serpent);
+			tirage_necessaire=1;
 		}
-		deplacer_serpent(serpent, nouvelle_direction);
+		srand(time(NULL));
+			nouvelle_direction = get_direction() ;
+			/*ignorer les demi tour */
+			switch(nouvelle_direction){
+				case 0:
+					if(serpent -> direction ==3) nouvelle_direction =3;
+					break;
+				case 1:
+					if(serpent -> direction ==2) nouvelle_direction =2;
+					break;
+				case 2:
+					if(serpent -> direction ==1) nouvelle_direction =1;
+					break;
+				case 3:
+					if(serpent -> direction ==0) nouvelle_direction =0;
+					break;
+				default : 
+					nouvelle_direction= serpent ->direction;
+			}
+			deplacer_serpent(serpent, nouvelle_direction);
+			if(tirage_necessaire){
+				grille_tirage_fruit (jeu_grille);
+				tirage_necessaire =0;
+			}
+		
 	}
 	
 return EXIT_SUCCESS;
